ced805f7f0303c0bd10864a3f574e9342562930f
CHNW <cnw.first@gmail.com>
2021-06-25 19:32:49 +0700

    googlemap

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id = "app">
+    <Map/>
+  </div>
+  <Map/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Map from './components/Map.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Map
   }
 }
 </script>
