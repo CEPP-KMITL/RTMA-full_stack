47332a830eba9e293e8c48bc72be7f9aebd0603a
T-Pakorn <Pakorn.Thanaprachanon@outlook.com>
2021-06-30 23:43:48 +0700

    Hot fix path

--- scraping/dist/index.js
+++ scraping/dist/index.js
@@ -1,16 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var twitter_1 = require("./puppeteer/twitter/twitter");
-// const testMetaUrl: string =
-//   'https://www.thairath.co.th/news/local/central/2124297';
-// scrapeMetatags(testMetaUrl)!.then(function (result: Array<any>) {
-//   console.log(result);
-// });
-// scrapeThairatNews(
+var scrapeMetatags_1 = require("./scrapeMetatags");
+var thairat_1 = require("./puppeteer/thairat/thairat");
+var testMetaUrl = 'https://www.thairath.co.th/news/local/central/2124297';
+scrapeMetatags_1.scrapeMetatags(testMetaUrl).then(function (result) {
+    console.log(result);
+});
+thairat_1.scrapeThairatNews('https://www.thairath.co.th/news/local/central/2126296').then(function (result) {
+    console.log(result);
+});
+// scrapeTwitterNews(
 //   'https://www.thairath.co.th/news/local/central/2124297'
 // )!.then(function (result) {
 //   console.log(result);
 // });
-twitter_1.scrapeTwitterNews('https://www.thairath.co.th/news/local/central/2124297').then(function (result) {
-    console.log(result);
-});
--- scraping/src/index.ts
+++ scraping/src/index.ts
@@ -2,21 +2,21 @@ import { scrapeMetatags } from './scrapeMetatags';
 import { scrapeThairatNews } from './puppeteer/thairat/thairat';
 import { scrapeTwitterNews } from './puppeteer/twitter/twitter';
 
-// const testMetaUrl: string =
-//   'https://www.thairath.co.th/news/local/central/2124297';
+const testMetaUrl: string =
+  'https://www.thairath.co.th/news/local/central/2124297';
 
-// scrapeMetatags(testMetaUrl)!.then(function (result: Array<any>) {
-//   console.log(result);
-// });
+scrapeMetatags(testMetaUrl)!.then(function (result: Array<any>) {
+  console.log(result);
+});
 
-// scrapeThairatNews(
+scrapeThairatNews(
+  'https://www.thairath.co.th/news/local/central/2126296'
+)!.then(function (result) {
+  console.log(result);
+});
+
+// scrapeTwitterNews(
 //   'https://www.thairath.co.th/news/local/central/2124297'
 // )!.then(function (result) {
 //   console.log(result);
 // });
-
-scrapeTwitterNews(
-  'https://www.thairath.co.th/news/local/central/2124297'
-)!.then(function (result) {
-  console.log(result);
-});
