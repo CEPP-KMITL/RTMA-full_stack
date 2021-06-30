15b0db80c1f99e5ee9d1cc4bad60a6b348d82a80
CHNW <cnw.first@gmail.com>
2021-07-01 01:09:50 +0700

    Update 1
    -Compatible with vue3

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,35 @@
-<template>
-  <div id = "app">
-    <Map/>
-  </div>
-  <Map/>
-</template>
-
 <script>
-import Map from './components/Map.vue'
-
+/* eslint-disable no-undef */
+import { computed, ref, onMounted} from 'vue'
+import { useGeolocation } from './useGeolocation'
+import { Loader } from '@googlemaps/js-api-loader'
+const GOOGLE_MAPS_API_KEY = 'AIzaSyD5OVCmPbVf6YZv6XRpN3NEfI1PzzOwBcU'
 export default {
   name: 'App',
-  components: {
-    Map
+  setup() {
+    const { coords } = useGeolocation()
+    const currPos = computed(() => ({
+      lat: coords.value.latitude,
+      lng: coords.value.longitude
+    }))
+    const loader = new Loader({ apiKey: GOOGLE_MAPS_API_KEY })
+    const mapDiv = ref(null)
+    onMounted(async () => {
+      await loader.load()
+      new google.maps.Map(mapDiv.value, {
+        center: {lat: 13, lng: 100},
+        zoom: 7
+      })
+    })
+    return { currPos, mapDiv }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <div>
+    <div ref="mapDiv" style="height: 100%;position: absolute; top: 0; bottom: -200px; left: 0; right: 0; z-index: 0;" />
+  </div>
+  
+</template>
+
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,6 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
-import VueGeolocation from 'vue-browser-geolocation'
 
-Vue.config.productionTip = false
-Vue.use(VueGeolocation)
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-new Vue({
-    render: h => h(App),
-}).$mount('#app')
-
+createApp(App).mount('#app')
