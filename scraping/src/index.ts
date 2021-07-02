1fd05e7a00e8547dfa835575f05f0f2ae168e553
T-Pakorn <Pakorn.Thanaprachanon@outlook.com>
2021-07-02 20:50:09 +0700

    Fix ts-node-dev bug

--- scraping/src/index.ts
+++ scraping/src/index.ts
@@ -5,20 +5,18 @@ import { scrapeTwitterNews } from './puppeteer/twitter';
 const testMetaUrl: string =
   'https://www.thairath.co.th/news/local/central/2124297';
 
-console.log('Test');
+// scrapeMetatags(testMetaUrl)!.then(function (result: Array<any>) {
+//   console.log(result);
+// });
 
-scrapeMetatags(testMetaUrl)!.then(function (result: Array<any>) {
-  console.log(result);
-});
+// scrapeThairatNews(
+//   'https://www.thairath.co.th/news/local/central/2126296',
+// )!.then(function (result) {
+//   console.log(result);
+// });
 
-scrapeThairatNews(
-  'https://www.thairath.co.th/news/local/central/2126296',
-)!.then(function (result) {
-  console.log(result);
-});
-
-scrapeTwitterNews(
-  'https://www.thairath.co.th/news/local/central/2124297',
-)!.then(function (result) {
-  console.log(result);
-});
+// scrapeTwitterNews(
+//   'https://www.thairath.co.th/news/local/central/2124297',
+// )!.then(function (result) {
+//   console.log(result);
+// });
