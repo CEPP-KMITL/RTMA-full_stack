ea16fc0c36f7f2f49178269f449b4f16a600c85a
T-Pakorn <Pakorn.Thanaprachanon@outlook.com>
2021-07-02 20:59:36 +0700

    Fix testing

---
+++ scraping/dist/config/config.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+exports.CONFIG = void 0;
+exports.CONFIG = {
+    RTMA_EMAIL_USERNAME: 'rtma_bot@outlook.com',
+    RTMA_EMAIL_PASSWORD: 'rtmabot123456',
+    RTMA_TWITTER_USERNAME: '@RTMA42232401',
+    RTMA_TWITTER_PASSWORD: 'rtmabottwitter123456'
+};
