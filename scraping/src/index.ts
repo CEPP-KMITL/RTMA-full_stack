30dc240300535e408e15461349ce81f1e4c33548
T-Pakorn <Pakorn.Thanaprachanon@outlook.com>
2021-07-02 20:54:40 +0700

    Update index.ts

--- scraping/src/index.ts
+++ scraping/src/index.ts
@@ -5,15 +5,15 @@ import { scrapeTwitterNews } from './puppeteer/twitter';
 const testMetaUrl: string =
   'https://www.thairath.co.th/news/local/central/2124297';
 
-// scrapeMetatags(testMetaUrl)!.then(function (result: Array<any>) {
-//   console.log(result);
-// });
+scrapeMetatags(testMetaUrl)!.then(function (result: Array<any>) {
+  console.log(result);
+});
 
-// scrapeThairatNews(
-//   'https://www.thairath.co.th/news/local/central/2126296',
-// )!.then(function (result) {
-//   console.log(result);
-// });
+scrapeThairatNews(
+  'https://www.thairath.co.th/news/local/central/2126296',
+)!.then(function (result) {
+  console.log(result);
+});
 
 // scrapeTwitterNews(
 //   'https://www.thairath.co.th/news/local/central/2124297',
