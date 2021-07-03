d02339127e3251c741bf2e9a34ea585edef1e6c3
Nichakan Sukhumjitpitayotai <67417910+moondamon@users.noreply.github.com>
2021-07-03 14:38:02 +0700

    incidentrawmodel

---
+++ backend/src/models/incidentRawModel.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const incidentRawSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    require: [true, 'Incident must have title'],
+  },
+  body: {
+    type: String,
+    require: [true, 'Incident must have body'],
+  },
+});
+
+const IncidentRaw = mongoose.model('Incident', incidentRawSchema);
+export { IncidentRaw };
