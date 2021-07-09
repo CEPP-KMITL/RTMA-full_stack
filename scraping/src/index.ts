569e961ddf3c37f025aab26694ad8a6bed5db99f
T-Pakorn <Pakorn.Thanaprachanon@outlook.com>
2021-07-09 19:50:50 +0700

    Add Sced and API

--- scraping/src/index.ts
+++ scraping/src/index.ts
@@ -1,17 +1,17 @@
-import { scrapeMetatags } from './scrapeMetatags';
 import { scrapeThairatNews } from './puppeteer/thairat';
 
 const testMetaUrl: string =
   'https://www.thairath.co.th/news/local/northeast/2128251';
 
-// scrapeThairatNews(
-//   'https://www.thairath.co.th/news/local/northeast/2128251',
-// )!.then(function (result) {
-//   console.log(result);
-// });
+setInterval(runScrapeThairatNews, 60000);
 
-scrapeThairatNews(
-  'https://www.thairath.co.th/tags/%E0%B8%AD%E0%B8%B8%E0%B8%9A%E0%B8%B1%E0%B8%95%E0%B8%B4%E0%B9%80%E0%B8%AB%E0%B8%95%E0%B8%B8',
-)!.then(function (result) {
-  console.log(result);
-});
+function runScrapeThairatNews() {
+  scrapeThairatNews(
+    'https://www.thairath.co.th/tags/%E0%B8%AD%E0%B8%B8%E0%B8%9A%E0%B8%B1%E0%B8%95%E0%B8%B4%E0%B9%80%E0%B8%AB%E0%B8%95%E0%B8%B8',
+  )!.then(function (result) {
+    console.log(result);
+    fetch('API_URL', { method: 'POST', body: result })
+      .then((result) => result.json())
+      .then(console.log);
+  });
+}
