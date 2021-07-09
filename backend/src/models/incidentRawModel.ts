bb338c168d15eb71d836398c05be0e987772c59a
Nichakan Sukhumjitpitayotai <67417910+moondamon@users.noreply.github.com>
2021-07-09 11:48:49 +0700

    apirawandeditschema

--- backend/src/models/incidentRawModel.ts
+++ backend/src/models/incidentRawModel.ts
@@ -1,16 +1,36 @@
 import mongoose from 'mongoose';
 
+const BodyAccidentSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    require: [true, 'Incident must have id'],
+    unique: true
+  }
+},
+{strict:false}
+);
+
 const incidentRawSchema = new mongoose.Schema({
   title: {
     type: String,
     require: [true, 'Incident must have title'],
-    unique: true
+  },
+  search_keyword:{
+    type: String,
+    require: [true, 'Incident must have search_keyword'],
   },
   body: {
-    type: String,
+    type: BodyAccidentSchema,
     require: [true, 'Incident must have body'],
   },
-});
+  packaging_timestamp:{
+    type: String,
+    require: [true, 'Incident must have packaging_timestamp'],
+  }
+},
+{strict:false}
+);
 
-const IncidentRaw = mongoose.model('Incident', incidentRawSchema);
+
+const IncidentRaw = mongoose.model('IncidentRaw', incidentRawSchema);
 export { IncidentRaw };
