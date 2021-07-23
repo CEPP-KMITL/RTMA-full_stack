b07d68671a7677f69cad6e21d7378d20f9a29e4d
T-Pakorn <Pakorn.Thanaprachanon@outlook.com>
2021-07-23 22:42:43 +0700

    Update Build Docker.

---
+++ backend/dist/routes/incidentsRoute.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.__esModule = true;
+var express_1 = require("express");
+var incidentsController_1 = require("../controllers/incidentsController");
+var authProtect = require('../middleware/authMiddleware');
+var router = express_1.Router();
+router.route('/patchIncident/:id').patch(authProtect, incidentsController_1.updateIncident);
+router.route('/deleteIncident/:id')["delete"](authProtect, incidentsController_1.deleteIncident);
+router.route('/postIncident').post(authProtect, incidentsController_1.createIncident);
+router.get('/getAllIncidents', incidentsController_1.getAllIncidents);
+router.get('/getIncident/:id', incidentsController_1.getIncident);
+exports["default"] = router;
