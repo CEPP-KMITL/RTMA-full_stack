dbd9664f6a37eb4fdc5c934c6549b4d09f97962d
Nichakan Sukhumjitpitayotai <67417910+moondamon@users.noreply.github.com>
2021-07-31 19:24:41 +0700

    addrouteoneday

--- backend/src/routes/incidentsRoute.ts
+++ backend/src/routes/incidentsRoute.ts
@@ -4,6 +4,7 @@ import {
   getAllIncidents,
   updateIncident,
   deleteIncident,
+  getonedayincident,
   getIncident,
 } from '../controllers/incidentsController';
 const authProtect = require('../middleware/authMiddleware');
@@ -13,6 +14,7 @@ router.route('/patchIncident/:id').patch(authProtect, updateIncident);
 router.route('/deleteIncident/:id').delete(authProtect, deleteIncident);
 router.route('/postIncident').post(authProtect, createIncident);
 router.get('/getAllIncidents', getAllIncidents);
+router.get('/getOneDay', getonedayincident);
 router.get('/getIncident/:id', getIncident);
 
 export default router;
