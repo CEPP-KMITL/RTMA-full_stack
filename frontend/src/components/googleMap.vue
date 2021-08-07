65e855625a93c9be0d3bcc26ec6e32b50ffefa6f
CHNW <cnw.first@gmail.com>
2021-08-07 14:57:41 +0700

    using api
    - able to fetch data from getOneDay incidents API

--- frontend/src/components/googleMap.vue
+++ frontend/src/components/googleMap.vue
@@ -13,6 +13,7 @@
       "
     />
     <div
+      id="myMenu"
       style="
         position: relative;
         z-index: 3;
@@ -32,11 +33,13 @@
             <hBtn></hBtn>
           </span>
           <span
+            id="productName"
             style="
               font-family: Prompt;
               font-weight: 750;
               font-size: 30px;
               margin-left: 8px;
+              color: #f1ece3;
             "
             >SONAR</span
           >
@@ -44,8 +47,9 @@
         </button>
       </div>
       <div>
-        <button class="myButton" :class="{ selected: isSelected }">
+        <button id="nearme" class="myButton" :class="{ selected: isSelected }">
           <span
+            id="nearmeIcon"
             class="material-icons"
             style="
               font-size: 28px;
@@ -56,17 +60,21 @@
           >
             radar
           </span>
-          <span style="font-family: Prompt; font-weight: 400">Near Me</span>
+          <span id="nearmeLabel" style="font-family: Prompt; font-weight: 400"
+            >Near Me</span
+          >
         </button>
       </div>
       <!-- Layer Button -->
       <div style="width: 180px">
         <button
+          id="layer"
           class="myButton"
           :class="{ selected: isSelected }"
           @click="convertLayer()"
         >
           <span
+            id="layerIcon"
             class="material-icons"
             style="
               font-size: 28px;
@@ -77,15 +85,21 @@
           >
             layers
           </span>
-          <span style="font-family: Prompt; font-weight: 400">Layer</span>
-          <span class="material-icons" style="margin-left: 60px">
+          <span id="layerLabel" style="font-family: Prompt; font-weight: 400"
+            >Layer</span
+          >
+          <span
+            id="layerArrow"
+            class="material-icons"
+            style="margin-left: 60px"
+          >
             chevron_right
           </span>
-          <span style="margin-top: 8px; margin-left: 12px; margin-bottom: 7px" 
+          <span style="margin-top: 8px; margin-left: 12px; margin-bottom: 7px"
             ><button
               class="layerButton"
               :class="{ selected: layerSelected }"
-              @click="convertLayer(),toggleScatter()"
+              @click="convertLayer(), toggleScatter()"
             >
               <span
                 class="material-icons"
@@ -95,7 +109,6 @@
                   margin-right: 8px;
                   color: #222831;
                 "
-                
               >
                 fmd_good
               </span>
@@ -105,7 +118,7 @@
             ><button
               class="layerButton"
               :class="{ selected: layerSelected }"
-              @click="convertLayer(),toggleHeat()"
+              @click="convertLayer(), toggleHeat()"
             >
               <span
                 class="material-icons"
@@ -143,7 +156,45 @@
       </div>
       <!-- DashBoard Button -->
       <div>
-        <button class="myButton" :class="{ selected: isSelected }">
+        <button
+          id="dashboard"
+          class="myButton"
+          :class="{ selected: isSelected }"
+        >
+          <span
+            id="dashboardIcon"
+            class="material-icons"
+            style="
+              font-size: 28px;
+              margin-left: 4px;
+              margin-right: 8px;
+              color: #222831;
+            "
+          >
+            layers
+          </span>
+          <span
+            id="dashboardLabel"
+            style="font-family: Prompt; font-weight: 400"
+            >Dashboard</span
+          >
+          <span
+            id="dashboardArrow"
+            class="material-icons"
+            style="margin-left: 8px"
+            @click="layerSelected = !layerSelected"
+          >
+            chevron_right
+          </span>
+        </button>
+      </div>
+      <div>
+        <button
+          id="darkmodeBtn"
+          class="myButton"
+          :class="{ selected: isSelected }"
+          @click="toggleMapTheme(this.currMapId), themeSwitch(this.curTheme)"
+        >
           <span
             class="material-icons"
             style="
@@ -155,7 +206,7 @@
           >
             layers
           </span>
-          <span style="font-family: Prompt; font-weight: 400">Dashboard</span>
+          <span style="font-family: Prompt; font-weight: 400">Dark Mode</span>
           <span
             class="material-icons"
             style="margin-left: 8px"
@@ -170,7 +221,7 @@
 </template>
 
 <script>
-import { reactive, defineComponent, computed, onMounted } from 'vue';
+import { defineComponent, computed } from 'vue';
 import { useGeolocation } from '/src/useGeolocation.js';
 import { Loader } from '@googlemaps/js-api-loader';
 import { GoogleMapsOverlay } from '@deck.gl/google-maps';
@@ -179,9 +230,9 @@ import { ScatterplotLayer } from '@deck.gl/layers';
 import { HexagonLayer } from '@deck.gl/aggregation-layers';
 import hamburgerBtn from './hamburgerBtn.vue';
 import searchbar from './searchBar.vue';
+import axios from 'axios';
 
 const GOOGLE_MAPS_API_KEY = 'AIzaSyD5OVCmPbVf6YZv6XRpN3NEfI1PzzOwBcU';
-
 export default defineComponent({
   components: {
     hBtn: hamburgerBtn,
@@ -198,19 +249,52 @@ export default defineComponent({
         lng: coords.value.longitude,
       })),
       sourceData: './gundata.json',
-      myMap : null,
+      myMap: null,
       loader: new Loader({
         apiKey: GOOGLE_MAPS_API_KEY,
         libraries: ['places'],
-        // myMap: null,
-        // overlay: null,
-        // scatterplot: null,
       }),
       scatterStatus: false,
       heatStatus: false,
+      currMapId: '77019c2d28bf4b75',
+      curTheme: 'dark',
+      accidentsData: [],
     };
   },
   methods: {
+    themeSwitch(theme) {
+      console.log('prev theme : ' + theme);
+      if (theme == 'dark') {
+        document.getElementById('productName').style.color = '#222831';
+        document.getElementById('nearme').style.background = '#4D6180';
+        document.getElementById('nearmeIcon').style.color = '#F1ECE3';
+        document.getElementById('nearmeLabel').style.color = '#F1ECE3';
+        document.getElementById('layer').style.background = '#4D6180';
+        document.getElementById('layerIcon').style.color = '#F1ECE3';
+        document.getElementById('layerLabel').style.color = '#F1ECE3';
+        document.getElementById('layerArrow').style.color = '#F1ECE3';
+        document.getElementById('dashboard').style.background = '#4D6180';
+        document.getElementById('dashboardIcon').style.color = '#F1ECE3';
+        document.getElementById('dashboardLabel').style.color = '#F1ECE3';
+        document.getElementById('dashboardArrow').style.color = '#F1ECE3';
+        this.curTheme = 'light';
+      } else if (theme == 'light') {
+        document.getElementById('productName').style.color = '#F1ECE3';
+        document.getElementById('nearme').style.background = '#F2B963';
+        document.getElementById('nearmeIcon').style.color = '#222831';
+        document.getElementById('nearmeLabel').style.color = '#222831';
+        document.getElementById('layer').style.background = '#F2B963';
+        document.getElementById('layerIcon').style.color = '#222831';
+        document.getElementById('layerLabel').style.color = '#222831';
+        document.getElementById('layerArrow').style.color = '#222831';
+        document.getElementById('dashboard').style.background = '#F2B963';
+        document.getElementById('dashboardIcon').style.color = '#222831';
+        document.getElementById('dashboardLabel').style.color = '#222831';
+        document.getElementById('dashboardArrow').style.color = '#222831';
+        this.curTheme = 'dark';
+      }
+      console.log('hi from themeswitch : ' + theme);
+    },
     convertSelect() {
       //   console.log("isSelected toggle");
       return (this.isSelected = !this.isSelected);
@@ -223,30 +307,41 @@ export default defineComponent({
       return (this.layerSelected = !this.layerSelected);
     },
     toggleScatter() {
-        this.scatterStatus = !this.scatterStatus;
-        // console.warn(this.scatterStatus);
-        this.initMap();
+      this.scatterStatus = !this.scatterStatus;
+      // console.warn(this.scatterStatus);
+      this.renderLayer();
     },
     toggleHeat() {
-        this.heatStatus = !this.heatStatus;
-        // console.warn(this.scatterStatus);
-        this.initMap();
+      this.heatStatus = !this.heatStatus;
+      // console.warn(this.scatterStatus);
+      this.initMap();
     },
-    renderLayer(){
-        const scatterplot = () =>
+    toggleMapTheme(id) {
+      if (id == '77019c2d28bf4b75') {
+        this.currMapId = '142837500402f49f';
+      } else {
+        this.currMapId = '77019c2d28bf4b75';
+      }
+      console.warn(this.currMapId + ' is current MapId');
+      this.initMap();
+    },
+    // newRender(){
+    //     const deckOverlay = new deck,GoogleMapsOverlay
+    // },
+    renderLayer() {
+      const scatterplot = () =>
         new ScatterplotLayer({
           id: 'scatter',
-          data: this.sourceData,
+          data: this.accidentsData.getIncidents,
           opacity: 0.8,
           filled: true,
-          radiusMinPixels: 2,
-          radiusMaxPixels: 5,
+          radiusMinPixels: 5,
+          radiusMaxPixels: 13,
           getPosition: (d) => [d.longitude, d.latitude],
-          getFillColor: (d) =>
-            d.n_killed > 0 ? [200, 0, 40, 150] : [255, 140, 0, 100],
-          visible: this.scatterStatus,
+          getFillColor: (d) => [200, 0, 40, 150],
+          visible: true,
         });
-        
+
       const heatmap = () =>
         new HeatmapLayer({
           id: 'heat',
@@ -260,19 +355,18 @@ export default defineComponent({
         layers: [heatmap(), scatterplot()],
       });
       overlay.setMap(this.myMap);
-      overlay.setProps();
-      console.warn('Hi from renderLayer() and scatterStatus is ' + this.scatterStatus);
     },
     async initMap() {
       await this.loader.load();
       this.myMap = new google.maps.Map(document.getElementById('myMap'), {
-        center: { lat: 42.9855, lng: -85.6656 } /*bkk latlng = 13,100*/,
+        center: { lat: 13, lng: 100 } /*bkk latlng = 13,100*/,
         zoom: 7,
         streetViewControl: true,
         mapTypeControl: false,
         zoomControl: false,
         minZoom: 4,
         maxZoom: 15,
+        mapId: this.currMapId,
       });
       new google.maps.places.Autocomplete(
         document.getElementById('searchInput'),
@@ -286,21 +380,14 @@ export default defineComponent({
     },
   },
   mounted() {
+    fetch('http://localhost:8000/api/v1/incidents/getOneDay')
+      .then(res => res.json())
+      .then(data => {
+          this.accidentsData = data;
+          console.log('Data is : ',this.accidentsData.getIncidents);
+      })
+      .catch(err => console.warn(err));
     this.initMap();
   },
 });
 </script>
-
-const scatterplot = () => new ScatterplotLayer({ // new ScatterplotLayer({ id:
-'scatter', data: sourceData, opacity: 0.8, filled: true, radiusMinPixels: 2,
-radiusMaxPixels: 5, getPosition: (d) => [d.longitude, d.latitude], getFillColor:
-(d) => d.n_killed > 0 ? [200, 0, 40, 150] : [255, 140, 0, 100], }); // const
-heatmap = () => // new HeatmapLayer({ // id: 'heat', // data: sourceData, //
-getPosition: (d) => [d.longitude, d.latitude], // getWeight: (d) => d.n_killed +
-d.n_injured * 0.5, // radiusPixel: 60, // }); // const hexagon = () => // new
-HexagonLayer({ // id: 'hex', // data: sourceData, // getPosition: (d) =>
-[d.longitude, d.latitude], // getElevation: (d) => d.n_killed * 2 + d.n_injured,
-// elevationScale: 100, // }); // const overlay = new GoogleMapsOverlay({ //
-layers: [ // scatterplot(), // heatmap(), // // hexagon(), // ], // }); //
-overlay.setMap(myMap); // //
-overlay.setProps({layer:[scatterplot(),heatmap()]});
--- frontend/src/components/searchBar.vue
+++ frontend/src/components/searchBar.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <input class="searchBar" id="searchInput" type="text" placeholder="Enter Location..." style="item-align:center;">
+        <!-- <q-input rounded outlined v-model="text" id="searchInput" label="Rounded outlined"></q-input> -->
     </div>
 </template>
 
