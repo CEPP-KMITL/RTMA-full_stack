9c09fea2c0eb2bf408cdc0999e1bf03595d180e6
Nichakan Sukhumjitpitayotai <67417910+moondamon@users.noreply.github.com>
2021-08-13 22:25:20 +0700

    editmodel

--- backend/src/models/incidentModel.ts
+++ backend/src/models/incidentModel.ts
@@ -3,39 +3,39 @@ import mongoose from 'mongoose';
 const incidentSchema = new mongoose.Schema({
   type: {
     type: String,
-    require: [true, 'User must have a username'],
+    require: [true, 'information must have a type'],
   },
   formattedname: {
     type: String,
-    require: [true, 'User must have a username'],
+    require: [true, 'information must have a formattedname'],
   },
   content: {
     type: String,
-    require: [true, 'User must have a username'],
+    require: [true, 'information must have a content'],
   },
   link: {
     type: String,
-    require: [true, 'User must have a username'],
+    require: [true, 'information must have a link'],
   },
   date: {
     type: Date,
-    require: [true, 'User must have a username'],
+    require: [true, 'information must have a date'],
   },
   from: {
     type: String,
-    require: [true, 'User must have a username'],
+    require: [true, 'information must have a from'],
   },
   province: {
     type: String,
-    require: [true, 'User must have a username'],
+    require: [true, 'information must have aprovince'],
   },
   latitude: {
     type: Number,
-    require: [true, 'User must have a username'],
+    require: [true, 'information must have a latitude'],
   },
   longitude: {
     type: Number,
-    require: [true, 'User must have a username'],
+    require: [true, 'information must have a longitude'],
   },
 });
 
