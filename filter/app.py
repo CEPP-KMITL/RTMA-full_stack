28ad88d2f3e49df684cbbac5becc87a01c00feee
Dear <dear19052543@gmail.com>
2021-08-13 19:29:45 +0700

    hotfix getData & del comment

--- filter/app.py
+++ filter/app.py
@@ -1,4 +1,3 @@
-# import pandas as pd             #?-----
 import time
 from time import mktime
 from datetime import date
@@ -114,9 +113,7 @@ def getDateTime(sentence, dotIndex,date):
 
     try:
         timeStr = YMD+" "+hour+":"+minute+":00"
-        # timeStr = day+"/"+month+"/"+year+" "+hour+":"+minute
         print("time->", timeStr)
-        # print("->",timeStr)
         LocalTime = datetime.strptime(timeStr, "%Y-%m-%d %H:%M:%S")
         EpochSecond = mktime(LocalTime.timetuple())
         utcTime = datetime.utcfromtimestamp(EpochSecond).isoformat()
@@ -189,7 +186,6 @@ def getDateAndTimeThairuth(text,dateSentence):
 
     sentences = text.split(" ")
     day,month,year,hour,minute = "01","01","1000","00","00"
-    # print(sentences)
 
     for index in range (len(sentences)):
         if sentences[index].find("เมื่อเวลา")!= -1 and minute =="00" and hour=="00":
@@ -198,7 +194,6 @@ def getDateAndTimeThairuth(text,dateSentence):
             minute = sentences[index+1][dotCheck+1:dotCheck+3]
 
         if sentences[index].find("วันที่")!= -1 and year=="1000":
-            # print(sentences)
             day = sentences[index+1]
             month = allMonth.index(sentences[index+2][:4])+1
             if month<10:
@@ -213,9 +208,7 @@ def getDateAndTimeThairuth(text,dateSentence):
             return getDateTimeThairuth(dateSentence)
         else:
             timeStr = year+"-"+month+"-"+day+" "+hour+":"+minute+":00"
-            # timeStr = day+"/"+month+"/"+year+" "+hour+":"+minute
             print("time->" ,timeStr)
-            # print("->",timeStr)
             LocalTime = datetime.strptime(timeStr,"%Y-%m-%d %H:%M:%S")
             EpochSecond = mktime(LocalTime.timetuple())
             utcTime = datetime.utcfromtimestamp(EpochSecond).isoformat()
@@ -225,7 +218,6 @@ def getDateAndTimeThairuth(text,dateSentence):
         return getDateTimeThairuth(dateSentence)
 
 # *------------------------------------------------
-
 
 
 
@@ -253,8 +245,6 @@ def getDataFormGoogleAPI(place):
             response = requests.get(url)
             resp_json_payload = response.json()
             status = resp_json_payload['status']
-            # print(url)
-            # print(resp_json_payload)
             countRequestFail+=1
 
         if resp_json_payload['status'] == "OK":
@@ -287,10 +277,7 @@ def getDataFormGoogleAPI(place):
             newRespons = requests.get(newurl)
             new_resp_json_payload = newRespons.json()
             status = new_resp_json_payload['status']
-            # print(resp_json_payload)
-            # resp_json_payload['results'][0]['formatted_address']
             countRequestFail2+=1
-            # print(new_resp_json_payload)
         if resp_json_payload['status'] == "OK":
             province = new_resp_json_payload['results'][0]['address_components'][0]['short_name']
             if province in provinceInTH.keys():
@@ -332,13 +319,10 @@ def getplace(text,tag=""):
                 return response
 
         if placeIsStart:
-            # print("continue")
             place+=sentence
-        # if place!="ไม่ระบุ":
     if tag != "":
         response = getDataFormGoogleAPI(tag)
     else:
-        # response = getDataFormGoogleAPI("ลาดกระบัง")
         response = "This is not place"
 
     if response == "This is not place" :
@@ -357,15 +341,15 @@ def getData():
     #! local url
     # url = 'http://node-app:3000/api/v1/incidentsRaw/getAllIncidents'
     # url ='http://localhost:8000/api/v1/incidentsRaw/getAllIncidents'
-    # try:
-    response = requests.get(url)
-    resp_json_payload = response.json()
-    # print(resp_json_payload)
-    if resp_json_payload['message'] == "Get all current incidents successfully.":
-        return resp_json_payload
-    return {'message': 'Error', 'results': 0, 'getIncidents': []}
-    # except:
-        # return {'message': "Error can't get data", 'results': 0, 'getIncidents': []}
+    try:
+        response = requests.get(url)
+        resp_json_payload = response.json()
+        # print(resp_json_payload)
+        if resp_json_payload['message'] == "Get all current incidents successfully.":
+            return resp_json_payload
+        return {'message': 'Error', 'results': 0, 'getIncidents': []}
+    except:
+        return {'message': "Error can't get data", 'results': 0, 'getIncidents': []}
 
 def postTargetobj(myobj):
 
@@ -433,173 +417,8 @@ def mainLoop():
         print("\n\n")
 
 
-print("start")
 mainLoop()
-schedule.every(5).seconds.do(mainLoop)
+schedule.every(10).seconds.do(mainLoop)
 while True:
-    print("inloop")
     schedule.run_pending()
     time.sleep(1)
-
-
-# ? ---------------------- note ----------------------------
-
-
-# ! thairuth กับ twitter ต่างกันแค่ตอน getTime
-
-
-# response = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address=ถนนกิ่งแก้ว&key=AIzaSyDVCjXv1DAZgVwRCTkq3kNsrP-xhU4LVKs')
-
-# resp_json_payload = response.json()
-
-# print(resp_json_payload)
-
-# {'results': [{'address_components': [{'long_name': 'Thanon King Kaeo', 'short_name': 'Thanon King Kaeo', 'types': ['route']},
-#                                      {'long_name': 'Thailand', 'short_name': 'TH', 'types': ['country', 'political']}],
-#  *             'formatted_address': 'Thanon King Kaeo, Thailand',
-#               'geometry': {'bounds': {'northeast': {'lat': 13.7256722, 'lng': 100.7436084}, 'southwest': {'lat': 13.6341051, 'lng': 100.7083777}},
-#  *                          'location': {'lat': 13.6792373, 'lng': 100.7237136},
-#                            'location_type': 'GEOMETRIC_CENTER',
-#                            'viewport': {'northeast': {'lat': 13.7256722, 'lng': 100.7436084},
-#                                         'southwest': {'lat': 13.6341051, 'lng': 100.7083777}}},
-#              'place_id': 'ChIJQUrlxvVdHTERXbf8jMehXUc', 'types': ['route']
-#              }
-#              ],
-# ! 'status': 'OK'}
-
-# {'results': [{'address_components': [{'long_name': 'ทางหลวงพิเศษห
-# มายเลข 7', 'short_name': 'ทางหลวงพิเศษหมายเลข 7', 'types': ['rout
-# e']}, {'long_name': 'Thailand', 'short_name': 'TH', 'types': ['country', 'political']}], 'formatted_address': 'ทางหลวงพิเศษหมายเลข
-#  7 Thailand', 'geometry': {'bounds': {'northeast': {'lat': 13.7536743, 'lng': 101.1390878}, 'southwest': {'lat': 12.6850689, 'lng': 100.6196553}}, 'location': {'lat': 13.3374549, 'lng': 101.0232797}, 'location_type': 'GEOMETRIC_CENTER', 'viewport': {'northeast': {'lat': 13.7484856, 'lng': 101.4343104}, 'southwest': {'lat':
-# 12.9264242, 'lng': 100.612249}}}, 'partial_match': True, 'place_id': 'ChIJvU0S_8dhHTERZOd6c0sZIs4', 'types': ['route']}], 'status': 'OK'}
-# บนถนนมอเตอร์เวย์กม.ที่47+600ขาเข้าชลบุรีหมู่5ต.รทุกถังบรรจุปูนผง
-
-
-# ?--------------------------------------------------------------------------------------------------
-# *path to get province data['results'][0]['address_components'][0]['short_name']
-# {
-#     "plus_code": {
-#         "compound_code": "82PF+X8 Nong Ri, Chon Buri District, Chon Buri, Thailand",
-#         "global_code": "7P5382PF+X8"
-#     },
-#     "results": [
-#         {
-#             "address_components": [
-#                 {
-#                     "long_name": "Chon Buri",
-#                     "short_name": "จ.ชลบุรี",
-#                     "types": [
-#                         "administrative_area_level_1",
-#                         "political"
-#                     ]
-#                 },
-#                 {
-#                     "long_name": "Thailand",
-#                     "short_name": "TH",
-#                     "types": [
-#                         "country",
-#                         "political"
-#                     ]
-#                 }
-#             ],
-#             "formatted_address": "Chon Buri, Thailand",
-#             "geometry": {
-#                 "bounds": {
-#                     "northeast": {
-#                         "lat": 13.5893563,
-#                         "lng": 101.7168862
-#                     },
-#                     "southwest": {
-#                         "lat": 12.5086232,
-#                         "lng": 100.6470747
-#                     }
-#                 },
-#                 "location": {
-#                     "lat": 13.3611431,
-#                     "lng": 100.9846717
-#                 },
-#                 "location_type": "APPROXIMATE",
-#                 "viewport": {
-#                     "northeast": {
-#                         "lat": 13.5893563,
-#                         "lng": 101.7168862
-#                     },
-#                     "southwest": {
-#                         "lat": 12.5086232,
-#                         "lng": 100.6470747
-#                     }
-#                 }
-#             },
-#             "place_id": "ChIJPxDrgxewAjERH_3dUV-RDik",
-#             "types": [
-#                 "administrative_area_level_1",
-#                 "political"
-#             ]
-#         }
-#     ],
-#     "status": "OK"
-# }
-
-
-# {
-#     "plus_code": {
-#         "compound_code": "MPHF+MF Racha Thewa, Bang Phli District, Samut Prakan, Thailand",
-#         "global_code": "7P52MPHF+MF"
-#     },
-#     "results": [
-#         {
-#             "address_components": [
-#                 {
-#                     "long_name": "Samut Prakan",
-#                     "short_name": "จ.สมุทรปราการ",
-#                     "types": [
-#                         "administrative_area_level_1",
-#                         "political"
-#                     ]
-#                 },
-#                 {
-#                     "long_name": "Thailand",
-#                     "short_name": "TH",
-#                     "types": [
-#                         "country",
-#                         "political"
-#                     ]
-#                 }
-#             ],
-#             "formatted_address": "Samut Prakan, Thailand",
-#             "geometry": {
-#                 "bounds": {
-#                     "northeast": {
-#                         "lat": 13.7169169,
-#                         "lng": 100.9639206
-#                     },
-#                     "southwest": {
-#                         "lat": 13.4785244,
-#                         "lng": 100.4444578
-#                     }
-#                 },
-#                 "location": {
-#                     "lat": 13.5990961,
-#                     "lng": 100.5998319
-#                 },
-#                 "location_type": "APPROXIMATE",
-#                 "viewport": {
-#                     "northeast": {
-#                         "lat": 13.7169169,
-#                         "lng": 100.9639206
-#                     },
-#                     "southwest": {
-#                         "lat": 13.4785244,
-#                         "lng": 100.4444578
-#                     }
-#                 }
-#             },
-#             "place_id": "ChIJWV8GL4-h4jARCvzY7dWvlUE",
-#             "types": [
-#                 "administrative_area_level_1",
-#                 "political"
-#             ]
-#         }
-#     ],
-#     "status": "OK"
-# }
