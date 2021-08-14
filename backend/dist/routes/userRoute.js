03e3dda23660ca2c8762cc790b7405e804e4bde8
Pakorn Thanaprachanon <Pakorn.Thanaprachanon@outlook.com>
2021-08-15 01:00:12 +0700

    Fix node-app not building

--- backend/dist/routes/userRoute.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express_1 = require("express");
-var authController_1 = require("../controllers/authController");
-var router = express_1.Router();
-router.post('/signUp', authController_1.signUp);
-router.post('/login', authController_1.login);
-exports["default"] = router;
