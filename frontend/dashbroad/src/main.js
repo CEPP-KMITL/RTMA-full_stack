521cfe8c7c3abf6cacfa2a4d68f3a64d0b5b16d9
Dear <dear19052543@gmail.com>
2021-08-21 18:44:20 +0700

    update dashbroad

--- frontend/dashbroad/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import { Quasar } from 'quasar'
-import quasarUserOptions from './quasar-user-options'
-import store from './store'
-
-createApp(App).use(store).use(store).use(store).use(Quasar, quasarUserOptions).mount('#app')
