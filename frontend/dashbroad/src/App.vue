8ca647d56f68330358202cc80cc74d701f123e9b
Dear <dear19052543@gmail.com>
2021-08-21 18:44:20 +0700

    Revert "update dashbroad"
    This reverts commit 521cfe8c7c3abf6cacfa2a4d68f3a64d0b5b16d9.

---
+++ frontend/dashbroad/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+
+  <div class="q-ma-lg">
+    <dashbroad/>
+  </div>
+
+</template>
+
+<script>
+// import { ref } from 'vue'
+import store from './store/index'
+import dashbroad from './components/dashbroad.vue'
+
+export default {
+  name: 'LayoutDefault',
+  store,
+
+  components: {
+    dashbroad
+  },
+
+}
+</script>
