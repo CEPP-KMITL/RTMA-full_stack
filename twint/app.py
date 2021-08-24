57a2b806ed041abc68e4eefc8c7edc68b78922b3
Pakorn Thanaprachanon <Pakorn.Thanaprachanon@outlook.com>
2021-08-24 09:31:05 +0700

    Fix not found twitter id bug

--- twint/app.py
+++ twint/app.py
@@ -1,8 +1,7 @@
 from TwintClass import TwintSearch
 API_ENDPOINT = "http://node-app:3000/api/v1/incidentsRaw/postIncident"
-username_target = ["js100radio", "traffic_1197", "fm91trafficpro"]
+username_target = ["js100radio", "Traffic_1197", "fm91trafficpro"]
 search_string = ["รถชน", "ไฟไหม้", "อุบัติเหตุ", "ชนกัน",
                  "เพลิงไหม้", "ชนกับ", "ไฟไหม้", "เสียหลัก", "พุ่งชน"]
-#search_string = []
 print("START TWINT")
 scraping = TwintSearch(300, username_target, search_string, API_ENDPOINT)
